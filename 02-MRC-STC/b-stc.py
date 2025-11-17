6578d17e70d639256d7da4b6ba3f791d0703826e
Roy Michael <roy.mchl@gmail.com>
2025-11-17 19:05:38 +0200

    stc updates

--- 02-MRC-STC/b-stc.py
+++ 02-MRC-STC/b-stc.py
@@ -13,39 +13,49 @@ num_symbols = 100000
 
 
 def main():
-    s0_int, s0_symbols = _get_qpsk(num_symbols * 2)
-    s_mrc_int, s_mrc_symbols = _get_qpsk(num_symbols)
 
-    s_stc_symbols = s0_symbols.reshape(num_symbols, 2)
-    s_stc_int = s0_int.reshape(num_symbols, 2)
+    s_int, s_symbols = _get_qpsk(num_symbols)
+
+    # into a 2 x (num_symbols / 2) matrices
+    s_stc_symbols = np.vstack((s_symbols[::2], s_symbols[1::2])).T
+    s_stc_int = np.vstack((s_int[::2], s_int[1::2])).T
+    num_stc = num_symbols // 2
 
     # Define SNR range in dB for the simulation and plots
-    snr_db_range = np.arange(0, 40, 2)
+    snr_db_range = np.arange(0, 30, 2)
 
     ser_stc_array = np.zeros((len(snr_db_range)))
     ser_mrc_array = np.zeros((len(snr_db_range)))
 
     for snr_db_idx, snr_db in enumerate(snr_db_range):
+
         # Convert SNR from dB to linear and calculate required noise power
         snr_linear = 10 ** (snr_db / 10.0)
-        rho = 1. / np.sqrt(snr_linear)
+        rho = np.sqrt(0.5) / np.sqrt(snr_linear)  # Noise scaling factor
 
-        n_stc = np.array([_get_complex(num_symbols), np.conj(_get_complex(num_symbols))])  # AWGN
-        n_mrc = _get_complex(num_symbols, 2)
-        h = _get_complex(num_symbols, 2)  # Channels
+        # AWGN
+        n_stc = np.array([_get_complex(num_stc), np.conj(_get_complex(num_stc))])
+        n_mrc = _get_complex(num_symbols, col=2)
 
-        ha = np.array(
-            [[h[:, 0], np.conj(h[:, 1])], [h[:, 1], -np.conj(h[:, 0])]]).transpose()  # A 50000 * 2 * 2  matrix
+        # Channels
+        h_stc = _get_complex(num_stc, col=2) # Channel remains constant over two symbol periods
+        h_mrc = _get_complex(num_symbols, col=2)
 
-        # STC Received signals
-        y_stc = (ha @ s_stc_symbols[:, :, np.newaxis]).squeeze(axis=-1)
-        y_stc += (rho * n_stc).transpose()
+        H = np.array(
+            [
+                [h_stc[:, 0], np.conj(h_stc[:, 1])],
+                [h_stc[:, 1], -np.conj(h_stc[:, 0])]
+            ]).transpose()  # n_stc * 2 * 2  matrix
 
-        y_mrc = s_mrc_symbols.reshape(-1, 1) * h + rho * n_mrc  # Received signals
+        # STC Received signals (2x1 MISO)
+        y_stc = ((H / np.sqrt(2)) @ s_stc_symbols[:, :, np.newaxis]).squeeze(axis=-1)
+        y_stc += (rho * n_stc).T  # Add noise
 
-        # Equalization
-        s_mrc_hat = np.sum(np.conj(h) * y_mrc, axis=1) / np.sum((np.abs(h) ** 2), axis=1)
-        s_stc_hat = (np.linalg.inv(ha) @ y_stc[:, :, np.newaxis]).squeeze(axis=-1)
+        y_mrc = s_symbols.reshape(-1, 1) * h_mrc + rho * n_mrc  # MRC Received signals (1x2 SIMO)
+
+        # Equalization/Detection
+        s_mrc_hat = np.sum(np.conj(h_mrc) * y_mrc, axis=1) / np.sum((np.abs(h_mrc) ** 2), axis=1)
+        s_stc_hat = (np.linalg.inv(H) @ y_stc[:, :, np.newaxis]).squeeze(axis=-1)
 
         # Detections
         constellation = _to_symbols(np.arange(0, 4))
@@ -54,7 +64,7 @@ def main():
 
         # Count the errors and calculate SER
         ser_stc_array[snr_db_idx] = np.sum(s_stc_int != s_stc_detected) / num_symbols
-        ser_mrc_array[snr_db_idx] = np.sum(s_mrc_int != s_mrc_detected) / num_symbols
+        ser_mrc_array[snr_db_idx] = np.sum(s_int != s_mrc_detected) / num_symbols
 
     # --- Plotting ---
     plt.figure(figsize=(10, 6))
@@ -62,7 +72,7 @@ def main():
     plt.semilogy(snr_db_range, ser_mrc_array, "r--", label=f'MRC 1X2')
     plt.semilogy(snr_db_range, ser_stc_array, "b-", label=f'STC 2X1')
 
-    plt.title('Symbol Error Rate (SER) vs. SNR for QPSK with MRC')
+    plt.title('Symbol Error Rate (SER) vs. SNR for QPSK with MRC/STC')
     plt.xlabel("SNR (Es/N0) [dB]")
     plt.ylabel("Symbol Error Rate (SER)")
     plt.ylim(10 ** -5, 1)  # Set y-axis limits for log scale
